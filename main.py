b012d4a683455f35c9d04402cfd20517acfc276b
Michael Korobkov <korobkov200364@gmail.com>
2020-03-23 15:06:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
@@ -7,71 +8,174 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
+import pymysql
+from pymysql.cursors import DictCursor
 import uuid
 import hashlib
+import xml.dom.minidom
+
+def xml_parse(fileName):
+    doc = xml.dom.minidom.parse(fileName)
+    
+
 
 
 #setting screen config
 Config.set('graphics', 'resizable', True)
-Config.set('graphics', 'width', 720 // 2)
-Config.set('graphics', 'height', 1280 // 2)
+Config.set('graphics', 'width', 9*50)
+Config.set('graphics', 'height', 16*50)
+
+#uploading kv file
+Builder.load_string("""
+
+<AuthoriseScreen>:
+    FloatLayout:
+        canvas.before:
+            Color:
+                rgba: .95, .95, .97, 1
+            Rectangle:
+                pos: self.pos
+                size: self.size
+
+        Label:
+            text: 'Welcome to the chat!'
+            pos_hint: {'y': .4}
+            font_size: 24
+            color: .1, .1, .1, 1
+            font_name: 'font.ttf'
+
+        TextInput:
+            multiline: False
+            size_hint: None, None
+            size: 200, 35
+            hint_text: 'Логин'
+            pos_hint: {'y': .6, 'center_x': .5}
+            font_size: 20
+            font_name: 'font.ttf'
+
+        TextInput:
+            multiline: False
+            size_hint: None, None
+            size: 200, 35
+            hint_text: 'Пароль'
+            pos_hint: {'y': .5, 'center_x': .5}
+            password: True
+            password_mask: '*'
+            font_size: 20
+            font_name: 'font.ttf'
+
+        Button:
+            text: 'Войти'
+            size_hint: None, None
+            size: 100, 40
+            pos_hint: {'y': .35, 'center_x': .5}
+            font_size: 24
+            on_press: CheckUser()
+            background_normal: ''
+            background_color: 74/255, 118/255, 168/255, 1
+            font_name: 'font.ttf'
+
+        Label:
+            text: 'Нет аккаунта?'
+            pos_hint: {'y': -.22}
+            font_size: 16
+            color: .1, .1, .1, 1
+            font_name: 'font.ttf'
+
+        Button:
+            text: 'Зарегистрироваться!'
+            size_hint: None, None
+            size: 200, 40
+            pos_hint: {'y': .2, 'center_x': .5}
+            font_size: 18
+            on_press: root.manager.current = 'register'
+            background_normal: ''
+            background_color: 74/255, 118/255, 168/255, 1
+            font_name: 'font.ttf'
+
+<RegisterScreen>:
+    FloatLayout:
+        canvas.before:
+            Color:
+                rgba: .95, .95, .97, 1                
+            Rectangle:
+                pos: self.pos
+                size: self.size
+
+        Label:
+            text: 'Создать аккаунт'
+            pos_hint: {'center_x': .5, 'y': .4}
+            font_size: 24
+            color: .1, .1, .1, 1
+            font_name: 'font.ttf'
+
+        TextInput:
+            multiline: False
+            size_hint: None, None
+            size: 200, 35
+            hint_text: 'Логин'
+            pos_hint: {'y': .7, 'center_x': .5}
+            font_size: 20
+            font_name: 'font.ttf'
+
+        TextInput:
+            multiline: False
+            size_hint: None, None
+            size: 200, 35
+            hint_text: 'Пароль'
+            pos_hint: {'y': .6, 'center_x': .5}
+            password: True
+            password_mask: '*'
+            font_size: 20
+            font_name: 'font.ttf'
+
+        TextInput:
+            multiline: False
+            size_hint: None, None
+            size: 200, 35
+            hint_text: 'Повторите пароль'
+            pos_hint: {'y': .5, 'center_x': .5}
+            password: True
+            password_mask: '*'
+            font_size: 20
+            font_name: 'font.ttf'
+
+        Button:
+            text: 'Зарегистрироваться!'
+            size_hint: None, None
+            size: 175, 50
+            pos_hint: {'y': .35, 'center_x': .5}     
+            background_normal: ''
+            background_color: 74/255, 118/255, 168/255, 1
+            font_name: 'font.ttf'
+
+""")
 
 #connecting to database
-import pymysql
-from pymysql.cursors import DictCursor
+
+
+def CheckUser(self, instance):
+    connection = pymysql.connect(host='localhost', user='root', password='123', db='python_chat', charset='utf8mb4')
+    with connection:
+        cur = connection.cursor()
+        cur.execute("")
+
+    connection.close()
 
 class AuthoriseScreen(Screen):
-    def __init__(self, **kw):
-        super(AuthoriseScreen, self).__init__(**kw)
-
-        fl              = FloatLayout(size_hint=[1, 1])
+    pass
         
-        welcomeLb       = Label(text="Чат-нейм", pos_hint={"y": .4}, font_size=24)
 
-        loginLabel      = Label(text="Логин:", pos_hint={"y":.175, "x":-.175}, font_size=18)
-        loginInp        = TextInput(size_hint=[0.5, 0.05], pos_hint={"y": .60, "x": .25}, multiline=False)
-
-        passLabel       = Label(text="Пароль:", pos_hint={"y": .025, "x":-.16}, font_size=18)
-        passInp         = TextInput(size_hint=[0.5, 0.05], pos_hint={"y": .45, "x": .25}, multiline=False, password=True, password_mask="•")
-
-        logInButton     = Button(text="Войти", size_hint=[1/3, .07], pos_hint={"y": .3, "x": 1 - 2/3}, font_size=24, on_press=self.CheckUser)
-
-        regLabel        = Label(text='Нет аккаунта?', pos_hint={"y":-.3 }, font_size=16)
-        regButton       = Button(text="Зарегистрироваться", size_hint=[.5, .07], pos_hint={"y": .1, "x": .25}, font_size=18, on_press=self.CheckUser)
-
-        fl.add_widget(welcomeLb)
-        fl.add_widget(loginLabel)
-        fl.add_widget(loginInp)
-        fl.add_widget(passLabel)
-        fl.add_widget(passInp)
-        fl.add_widget(logInButton)
-        fl.add_widget(regLabel)
-        fl.add_widget(regButton)
-
-        self.add_widget(fl)
-
-    def CheckUser(self, instance):
-        connection = pymysql.connect(host='localhost', user='root', password='123', db='python_chat', charset='utf8mb4')
-        with connection:
-            cur = connection.cursor()
-            cur.execute("YOUR REQUEST HERE")
-
-        connection.close()
-
-    def RegisterUser(self, instance):
-        connection = pymysql.connect(host='localhost', user='root', password='123', db='python_chat', charset='utf8mb4')
-        with connection:
-            cur = connection.cursor()
-            cur.execute("YOUR REQUEST HERE")
-
-        connection.close()
+class RegisterScreen(Screen):
+    pass
 
 mainScreenManager = ScreenManager()
-mainScreenManager.add_widget(AuthoriseScreen(name="auth"))
+mainScreenManager.add_widget(AuthoriseScreen(name='auth'))
+mainScreenManager.add_widget(RegisterScreen(name='register'))
 
-class KivyApp(App):
+class ChatApp(App):
     def build(self):
         return mainScreenManager
 
 if __name__ == '__main__':
-    KivyApp().run()
+    ChatApp().run()
